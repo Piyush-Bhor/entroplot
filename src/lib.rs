614f3477737fe64ded7150dce8baa0361b8c1ee1
Piyush Bhor <piyushbhor22@gmail.com>
2025-09-21 04:05:13 -0400

    Swapped custom entropy function with crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,6 @@
+use entropy::shannon_entropy;
 use std::fs::File;
 use std::io::Read;
-
-// Algorithm to calculate Shannon entropy
-fn entropy(data: &[u8]) -> f64 {
-    let mut counts = [0usize; 256];
-    for &b in data {
-        counts[b as usize] += 1;
-    }
-    let len = data.len() as f64;
-    counts
-        .iter()
-        .filter(|&&c| c > 0)
-        .map(|&c| {
-            let p = c as f64 / len;
-            -p * p.log2()
-        })
-        .sum()
-}
 
 pub fn read_file(file_path: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let mut file = File::open(file_path)?;
@@ -26,11 +10,13 @@ pub fn read_file(file_path: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>>
     Ok(buf)
 }
 
-pub fn calculate_entropy(buf: &[u8], block_size: usize) -> Vec<f64> {
-    buf.chunks(block_size).map(|chunk| entropy(chunk)).collect()
+pub fn calculate_entropy(buf: &[u8], block_size: usize) -> Vec<f32> {
+    buf.chunks(block_size)
+        .map(|chunk| shannon_entropy(chunk))
+        .collect()
 }
 
-pub fn draw_entropy_graph(entropies: &[f64]) {
+pub fn draw_entropy_graph(entropies: &[f32]) {
     for &h in entropies {
         let bars = (h.round() as usize).min(8);
         println!("{:.2} | {}", h, "█".repeat(bars));
